788cb9b7181fe32f8ddac801bde47d93e3882e00
nuuuwan <nuuuwan@gmail.com>
2022-05-18 17:09:30 +0530

    Updated values in EXAMPLE_POLL_LIST

--- src/core/Poll.js
+++ src/core/Poll.js
@@ -7,18 +7,18 @@ export default class Poll {
 
 export const EXAMPLE_POLL_LIST = [
   new Poll("Should Sri Lanka abolish the Executive Presidency?", {
-    yes: "Yes - we should",
-    no: "No - we should not",
+    yes: "Yes",
+    no: "No",
   }),
   new Poll(
     "Should Sri Lanka abolish the 20th Amendment to the Constituition?",
     {
-      yes: "Yes - we should",
-      no: "No - we should not",
+      yes: "Yes",
+      no: "No",
     }
   ),
   new Poll("Should Sri Lanka have General Elections in the next 3 months?", {
-    yes: "Yes - we should",
-    no: "No - we should not",
+    yes: "Yes",
+    no: "No",
   }),
 ];
