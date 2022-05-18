507f43d2d85f826d301cfcbff35ddffdd29a71ca
nuuuwan <nuuuwan@gmail.com>
2022-05-18 17:41:15 +0530

    Added Alert

--- src/stateful/pages/HomePage.js
+++ src/stateful/pages/HomePage.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import * as React from "react";
 // import ReactGA from "react-ga";
 
+import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box";
 
 import { EXAMPLE_POLL_LIST } from "../../core/Poll.js";
@@ -48,6 +49,10 @@ export default class HomePage extends Component {
         <CustomAppBar />
 
         <PollView poll={activePoll} />
+
+        <Alert severity="info">
+          Voting with Phone and Email is still under development. Stay Tuned!
+        </Alert>
 
         <VersionWidget />
         <CustomBottomNavigation
