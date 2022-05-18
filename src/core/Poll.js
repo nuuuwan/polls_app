4023ba70405737d8287744bd4beac48aee0479bf
nuuuwan <nuuuwan@gmail.com>
2022-05-18 16:57:01 +0530

    Replaced EXAMPLE_POLL1 with EXAMPLE_POLL_LIST

--- src/core/Poll.js
+++ src/core/Poll.js
@@ -1,11 +1,24 @@
 export default class Poll {
-  constructor(question, answerList) {
+  constructor(question, answerValueToLabel) {
     this.question = question;
-    this.answerList = answerList;
+    this.answerValueToLabel = answerValueToLabel;
   }
 }
 
-export const EXAMPLE_POLL1 = new Poll(
-  "Should we abolish the Executive Presidency?",
-  ["Yes", "No"]
-);
+export const EXAMPLE_POLL_LIST = [
+  new Poll("Should Sri Lanka abolish the Executive Presidency?", {
+    yes: "Yes - we should",
+    no: "No - we should not",
+  }),
+  new Poll(
+    "Should Sri Lanka abolish the 20th Amendment to the Constituition?",
+    {
+      yes: "Yes - we should",
+      no: "No - we should not",
+    }
+  ),
+  new Poll("Should Sri Lanka have General Elections in the next 3 months?", {
+    yes: "Yes - we should",
+    no: "No - we should not",
+  }),
+];
