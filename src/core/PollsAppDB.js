949ae129de9d8a9ab240c3ea99c72d7b2b42e146
nuuuwan <nuuuwan@gmail.com>
2022-05-19 13:22:55 +0530

    Added PollsAppDB

--- src/core/PollsAppDB.js
+++ src/core/PollsAppDB.js
@@ -1,3 +1,36 @@
+import * as AWS from "aws-sdk";
+import { ConfigurationOptions } from "aws-sdk";
+import { AWSSecretConstants } from "../constants/SecretConstants.js";
+
 export default class PollsAppDB {
-  static async addPollResult(pollResult) {}
+  static init() {
+    const configuration: ConfigurationOptions = {
+      region: AWSSecretConstants.region,
+      secretAccessKey: AWSSecretConstants.secretAccessKey,
+      accessKeyId: AWSSecretConstants.accessKeyId,
+    };
+    AWS.config.update(configuration);
+  }
+
+  static getClient() {
+    return new AWS.DynamoDB.DocumentClient();
+  }
+
+  static addPollResult(pollResult) {
+    PollsAppDB.getClient().put(
+      {
+        TableName: "polls-app-poll-result",
+        Item: pollResult.dict,
+      },
+      function (err, data) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.debug("PollsAppDB.addPollResult complete!");
+        }
+      }
+    );
+  }
 }
+
+PollsAppDB.init();
