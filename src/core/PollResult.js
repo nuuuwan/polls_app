b175fe588b818ad8cff2965f738ac3c309884dd9
nuuuwan <nuuuwan@gmail.com>
2022-05-19 13:22:24 +0530

    Updated schema. Added pollResultID derived from pollID, userID and timeUpdated;

--- src/core/PollResult.js
+++ src/core/PollResult.js
@@ -1,15 +1,20 @@
 export default class PollResult {
-  constructor(userID, pollID, answer, timeUpdated) {
-    this.userID = userID;
+  constructor(pollID, userID, answer, timeUpdated) {
     this.pollID = pollID;
+    this.userID = userID;
     this.answer = answer;
     this.timeUpdated = timeUpdated;
   }
 
+  get pollResultID() {
+    return [this.pollID, this.userID, this.timeUpdated].join("-");
+  }
+
   get dict() {
     return {
-      userID: this.userID,
+      pollResultID: this.pollResultID,
       pollID: this.pollID,
+      userID: this.userID,
       answer: this.answer,
       timeUpdated: this.timeUpdated,
     };
