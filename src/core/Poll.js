2a5678ed4077ec110d899adf67fe7792725a3b02
nuuuwan <nuuuwan@gmail.com>
2022-05-20 09:20:17 +0530

    Reorder answerList

--- src/core/Poll.js
+++ src/core/Poll.js
@@ -11,20 +11,20 @@ export const EXAMPLE_POLL_LIST = [
   new Poll(
     "sl-exec-pres",
     "Should Sri Lanka abolish the Executive Presidency?",
-    ["Undecided", "Yes", "No"],
+    ["Yes", "No", "Undecided"],
     "Undecided"
   ),
   new Poll(
     "sl-20A",
     "Should Sri Lanka abolish the 20th Amendment to the Constituition?",
-    ["Undecided", "Yes", "No"],
+    ["Yes", "No", "Undecided"],
     "Undecided"
   ),
 
   new Poll(
     "sl-elec-2022",
     "Should Sri Lanka have General Elections in the next 3 months?",
-    ["Undecided", "Yes", "No"],
+    ["Yes", "No", "Undecided"],
     "Undecided"
   ),
 ];
