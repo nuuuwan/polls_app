20ded4ba2d480b0fec11b7a4d9547716cfb515fb
nuuuwan <nuuuwan@gmail.com>
2022-05-20 10:38:43 +0530

    Updated PollIDs

--- src/core/Poll.js
+++ src/core/Poll.js
@@ -9,34 +9,34 @@ export default class Poll {
 
 export const EXAMPLE_POLL_LIST = [
   new Poll(
-    "534d-cdad-c48e-3d14-7a8a",
+    "426e5664e22e9524c968715f30f6e325",
     "Should Sri Lanka abolish the Executive Presidency?",
     ["Yes", "No", "Undecided"],
     "Undecided"
   ),
   new Poll(
-    "ea35-d38c-1ce9-a1d2-5563",
+    "c7afc6cb3a5f62e158147de8b1aef6a0",
     "Should Sri Lanka abolish the 20th Amendment to the Constituition?",
     ["Yes", "No", "Undecided"],
     "Undecided"
   ),
 
   new Poll(
-    "e059-2040-f2b8-a613-998a",
+    "61006fc080178e15c2f89483da9b1934",
     "Should Sri Lanka have General Elections in the next 3 months?",
     ["Yes", "No", "Undecided"],
     "Undecided"
   ),
 
   new Poll(
-    "e67c-1165-2e00-71a6-9c04",
+    "f3d94e50567ce6d3aefe33ea50413931",
     "What should you put in this new poll?",
     ["Yes", "No", "Undecided"],
     "Undecided"
   ),
 
   new Poll(
-    "caa7-1eaf-37fc-bd24-3fa6",
+    "33157ba8ed69d87c659c761c80caaf6b",
     "What should you put in this one answer poll poll?",
     ["Yes", "No", "Undecided"],
     "Undecided"
