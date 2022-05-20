1aa2c184b494a187c274cdb500fd22c049942ebb
nuuuwan <nuuuwan@gmail.com>
2022-05-20 07:19:40 +0530

    Reverted colors;

--- src/nonstate/atoms/PercentageWidget.js
+++ src/nonstate/atoms/PercentageWidget.js
@@ -6,28 +6,26 @@ const STYLE = {
   height: HEIGHT,
 };
 
-export default function PercentageWidget({ answer, n, np }) {
-  const color = {
-    Yes: "#00534e",
-    No: "#8d153a",
-    Undecided: "#eb7400",
-  }[answer];
+const STYLE_INNER = {
+  height: HEIGHT,
+  backgroundColor: "#1976D2",
+  float: "left",
+  marginRight: "5%",
+  borderRadius: "10%",
+  opacity: 0.25,
+};
 
+export default function PercentageWidget({ n, np }) {
   const p = np / n;
   const pStr = parseInt(p * 100 + 0.5) + "%";
   const width = parseInt(p * 80 + 0.5) + "%";
-  const styleP = {
+  const styleInnerCustom = {
     width: width,
-    height: HEIGHT,
-    backgroundColor: color,
-    float: "left",
-    marginRight: "5%",
-    borderRadius: "10%",
   };
 
   return (
     <div style={STYLE}>
-      <span style={styleP} />
+      <span style={{ ...STYLE_INNER, ...styleInnerCustom }} />
       <Typography variant="body1" sx={{ fontWeight: "bold" }}>
         {pStr}
       </Typography>
