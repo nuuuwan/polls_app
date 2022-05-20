a1dc695c1096c3868681c40efb50c72f9bd2c60d
nuuuwan <nuuuwan@gmail.com>
2022-05-20 09:18:12 +0530

    Use StatisticsX.getErrorBounds; Added significance info;

--- src/nonstate/molecules/PollStatisticsView.js
+++ src/nonstate/molecules/PollStatisticsView.js
@@ -1,5 +1,47 @@
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import StatisticsX from "../../base/StatisticsX.js";
+export default function PollStatisticsView({
+  answerList,
+  totalVotes,
+  answerToVotes,
+}) {
+  const sortedAnswerStats = answerList
+    .map(function (answer) {
+      const answerVotes = answerToVotes[answer] ? answerToVotes[answer] : 0;
+      const { p, stdev } = StatisticsX.getErrorBounds(totalVotes, answerVotes);
+      return { answer, p, stdev };
+    })
+    .sort(function (answerA, answerB) {
+      return answerB.p - answerA.p;
+    });
 
-export default function PollStatisticsView({ totalVotes }) {
-  return <Typography variant="body2">{totalVotes + " votes"}</Typography>;
+  const topAnswerStats = sortedAnswerStats[0];
+  const nextAnswerStats = sortedAnswerStats[1];
+
+  let significanceStr = "too close to call";
+  let z, label;
+  for ([z, label] of [
+    [3, "very significantly"],
+    [2, "significantly"],
+    [1, "slightly"],
+  ]) {
+    const topLower = topAnswerStats.p - topAnswerStats.stdev * z;
+    const nextUpper = nextAnswerStats.p + nextAnswerStats.stdev * z;
+    const gap = topLower - nextUpper;
+    if (gap > 0) {
+      significanceStr = `"${topAnswerStats.answer}" leads ${label}`;
+      break;
+    }
+  }
+
+  return (
+    <Stack direction="column">
+      <Typography variant="caption" sx={{ color: "gray" }}>
+        {significanceStr}
+      </Typography>
+      <Typography variant="body2">{totalVotes + " votes"}</Typography>
+    </Stack>
+  );
 }
