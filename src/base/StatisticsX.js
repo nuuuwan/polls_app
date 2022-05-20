8431219a36ed63d8cb9f009c48455dec43b24fda
nuuuwan <nuuuwan@gmail.com>
2022-05-20 09:17:40 +0530

    Added StatisticsX with ErrorBounds

---
+++ src/base/StatisticsX.js
@@ -0,0 +1,11 @@
+export default class StatisticsX {
+  static getErrorBounds(n, np) {
+    const p = np / n;
+    const stdev = Math.sqrt(np * (1 - p)) / n;
+    const range = stdev * 2;
+    const [lower, upper] = [p - range, p + range].map((x) =>
+      Math.min(Math.max(x, 0), 1)
+    );
+    return { lower, upper, p, stdev };
+  }
+}
