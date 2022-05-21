02fd9a1c4a22b3d49d7f6525361a69c8f0fb18f2
nuuuwan <nuuuwan@gmail.com>
2022-05-21 09:42:22 +0530

    Implemented UserPage

--- src/stateful/pages/UserPage.js
+++ src/stateful/pages/UserPage.js
@@ -1,6 +1,59 @@
 import { Component } from "react";
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import ReactCountryFlag from "react-country-flag";
+
+import GeoLocationDBX from "../../base/GeoLocationDBX";
+import TitledBody from "../../nonstate/molecules/TitledBody";
+
+const STYLE_COUNTRY_FLAG = {
+  fontSize: "150%",
+  marginLeft: "1%",
+};
+
 export default class UserPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { geoInfo: null };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      geoInfo: await GeoLocationDBX.getInfo(),
+    });
+  }
+
   render() {
-    return "TODO-UserPage";
+    const { geoInfo } = this.state;
+    if (!geoInfo) {
+      return "Loading...";
+    }
+
+    const [lat, lng] = geoInfo.latLng;
+    const locationStr = `${lat}°N, ${lng}°E`;
+
+    return (
+      <Box>
+        <Typography variant="h4">User</Typography>
+        <TitledBody title="UserID" body={geoInfo.infoHash} />
+        <TitledBody title="IP" body={geoInfo.ipV4} />
+        <TitledBody title="Location" body={locationStr} />
+        <TitledBody
+          title="Country"
+          body={
+            <>
+              {geoInfo.countryCode}
+              <ReactCountryFlag
+                style={STYLE_COUNTRY_FLAG}
+                countryCode={geoInfo.countryCode}
+              />
+            </>
+          }
+        />
+      </Box>
+    );
   }
 }
