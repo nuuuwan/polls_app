c1ed13be314d5fd2fb2bbab7948941bb159d362c
nuuuwan <nuuuwan@gmail.com>
2022-05-21 10:45:29 +0530

    Added EquilateralPolygon

---
+++ src/nonstate/atoms/EquilateralPolygon.js
@@ -0,0 +1,28 @@
+import { DataStructures } from "@nuuuwan/utils-js-dev";
+
+export default function EquilateralPolygon({ cx, cy, r, n, color }) {
+  if (n >= 7) {
+    return (
+      <svg>
+        <circle cx={cx} cy={cy} r={r} fill={color} stroke="none" />
+      </svg>
+    );
+  }
+
+  const d =
+    DataStructures.range(0, n)
+      .map(function (i) {
+        const theta0 = -Math.PI / 2;
+        const theta = theta0 + (Math.PI * 2 * i) / n;
+        const x = parseInt(cx + r * Math.cos(theta));
+        const y = parseInt(cy + r * Math.sin(theta));
+        const label = i === 0 ? "M" : "L";
+        return label + x + "," + y;
+      })
+      .join("") + "Z";
+  return (
+    <svg>
+      <path d={d} fill={color} stroke="none" />
+    </svg>
+  );
+}
