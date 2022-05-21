95e5ce750275fd32fca3fedaf1345c405049fcea
nuuuwan <nuuuwan@gmail.com>
2022-05-21 19:26:24 +0530

    Added Caching

--- src/core/PollsAppServer.js
+++ src/core/PollsAppServer.js
@@ -1,23 +1,33 @@
+import Cache from "../base/Cache";
 import AWSDynamoDBX from "../base/AWSDynamoDBX";
-import Poll from "./Poll";
 import PollExtended from "./PollExtended";
 import PollResult from "./PollResult";
 
 export default class PollsAppServer {
   // Polls
-  static async getPollIDs() {
+  static async getPollIDsNoCache() {
     const data = await AWSDynamoDBX.generic({
       cmd: "multiget-ids-polls",
     });
     return data.map((d) => d["pollID"]);
   }
 
-  static async getPollExtended(pollID) {
+  static async getPollIDs() {
+    return await Cache.get("getPollIDs", PollsAppServer.getPollIDsNoCache);
+  }
+
+  static async getPollExtendedNoCache(pollID) {
     const d = await AWSDynamoDBX.generic({
       cmd: "get-poll-extended",
       id: pollID,
     });
     return PollExtended.fromDict(d);
+  }
+
+  static async getPollExtended(pollID) {
+    return await Cache.get("getPollExtended:" + pollID, async () =>
+      PollsAppServer.getPollExtendedNoCache(pollID)
+    );
   }
 
   // Poll Results
@@ -26,6 +36,7 @@ export default class PollsAppServer {
       cmd: "put-poll-result",
       d: PollResult.toDict(pollResult),
     });
+    Cache.clear("getPollExtended:" + pollResult.pollID);
     return PollResult.fromDict(d);
   }
 }
