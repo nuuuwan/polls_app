54722d0729a795980f9fd040f58cf6686561d2e6
nuuuwan <nuuuwan@gmail.com>
2022-05-21 10:46:04 +0530

    Use IDImage

--- src/stateful/pages/UserPage.js
+++ src/stateful/pages/UserPage.js
@@ -2,12 +2,14 @@ import { Component } from "react";
 import * as React from "react";
 
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
 import ReactCountryFlag from "react-country-flag";
 
 import GeoLocationDBX from "../../base/GeoLocationDBX";
 import TitledBody from "../../nonstate/molecules/TitledBody";
+import IDImage from "../../nonstate/atoms/IDImage";
 
 const STYLE_COUNTRY_FLAG = {
   fontSize: "150%",
@@ -38,6 +40,10 @@ export default class UserPage extends Component {
     return (
       <Box>
         <Typography variant="h4">User</Typography>
+        <Grid container justifyContent="center">
+          <IDImage id={geoInfo.infoHash} />
+        </Grid>
+
         <TitledBody title="UserID" body={geoInfo.infoHash} />
         <TitledBody title="IP" body={geoInfo.ipV4} />
         <TitledBody title="Location" body={locationStr} />
