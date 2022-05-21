e7c6385a90d141f787899169ec189d98d4eeed26
nuuuwan <nuuuwan@gmail.com>
2022-05-21 09:21:42 +0530

    Updated label

--- src/nonstate/molecules/CustomAppBarMenu.js
+++ src/nonstate/molecules/CustomAppBarMenu.js
@@ -37,7 +37,7 @@ export default function CustomAppBarMenu({ onSelectPage }) {
           onClick={onClickCode}
         />
         <CustomAppBarMenuItem
-          label="Help"
+          label="Help/FAQs"
           Icon={HelpIcon}
           onClick={(e) => onSelectPage(HelpPage)}
         />
