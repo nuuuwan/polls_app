2888c484b1082891356866c1a406df43757a1922
nuuuwan <nuuuwan@gmail.com>
2022-05-21 09:21:07 +0530

    Added basic implementation for HelpPage FAQs

--- src/stateful/pages/HelpPage.js
+++ src/stateful/pages/HelpPage.js
@@ -1,6 +1,33 @@
 import { Component } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import { URL_GITHUB_REPO_POLLS_APP } from "../../constants/Constants";
+import SimpleLink from "../../nonstate/atoms/SimpleLink";
+import FAQ from "../../nonstate/molecules/FAQ";
+
+const FAQS = [
+  {
+    question: "Where can I find the code for this App?",
+    answerParagraphs: [
+      <>
+        You can find the code for this app at
+        <SimpleLink href={URL_GITHUB_REPO_POLLS_APP} />.
+      </>,
+      "Feel free to fork the code, report issues, or ask questions.",
+    ],
+  },
+];
+
 export default class HelpPage extends Component {
   render() {
-    return "TODO-HelpPage";
+    return (
+      <Box>
+        <Typography variant="h4">FAQs</Typography>
+        {FAQS.map(function (faq, iFaq) {
+          return <FAQ key={"faq-" + iFaq} faq={faq} iFaq={iFaq} />;
+        })}
+      </Box>
+    );
   }
 }
