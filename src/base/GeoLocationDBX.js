b1ee8fa74ae7510bfb405df9ad42dfac222fceaa
nuuuwan <nuuuwan@gmail.com>
2022-05-21 13:05:17 +0530

    Added USE_RANDOM_SALT

--- src/base/GeoLocationDBX.js
+++ src/base/GeoLocationDBX.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import HashX from "./HashX";
 
 const URL_GEOLOCATION_DB = "https://geolocation-db.com/json/";
+const USE_RANDOM_SALT = true;
+const ID_SALT = "adb034fad7a9f45ebc20249cc85eae96";
 
 export default class GeoLocationDBX {
   static async getInfo() {
@@ -11,7 +13,8 @@ export default class GeoLocationDBX {
     const countryName = data.country_name;
     const ipV4 = data.IPv4;
     const latLng = [data.latitude, data.longitude];
-    const infoHash = HashX.md5({ countryCode, latLng, ipV4 });
+    const randomValue = USE_RANDOM_SALT ? Math.random() : ID_SALT;
+    const infoHash = HashX.md5({ countryCode, latLng, ipV4, randomValue });
     const userID = infoHash;
     return { countryCode, countryName, latLng, ipV4, infoHash, userID };
   }
