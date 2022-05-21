3360489e807386fbb828e99507f51c3a5cf3b5c6
nuuuwan <nuuuwan@gmail.com>
2022-05-21 10:55:30 +0530

    Added GoogleMapsLink

---
+++ src/nonstate/atoms/GoogleMapsLink.js
@@ -0,0 +1,7 @@
+import SimpleLink from "./SimpleLink";
+const URL_BASE = "https://www.google.com/maps/search/";
+
+export default function GoogleMapsLink({ searchText }) {
+  const href = URL_BASE + encodeURIComponent(searchText);
+  return <SimpleLink href={href} label={searchText} />;
+}
