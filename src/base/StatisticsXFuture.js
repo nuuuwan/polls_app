90a4c561f0b34c0264058d401b0e8569f7463e7c
nuuuwan <nuuuwan@gmail.com>
2022-05-21 12:55:11 +0530

    More bug fixes (Incomplete)

--- src/base/StatisticsXFuture.js
+++ src/base/StatisticsXFuture.js
@@ -9,18 +9,20 @@ export default class StatisticsXFuture {
     const N_STDEV = 2;
     const pObserved = npObserved / nObserved;
     const Q = 100;
-    let [lower, upper] = [0, 1];
+    let [lower, upper] = [1, 1];
     for (let i in DataStructures.range(0, Q + 1)) {
       const pActual = i / Q;
       const cdfForObserved = cdf(npObserved, nObserved, pActual);
 
-      if (lower === 0 && cdfForObserved < 1 - SIGNIFICANCE / 2) {
+      if (lower === 1 && cdfForObserved < 1 - SIGNIFICANCE / 2) {
         lower = pActual;
       }
       if (cdfForObserved >= SIGNIFICANCE / 2) {
         upper = pActual;
       }
     }
+
+    lower = Math.min(upper, lower);
 
     return { lower, upper, p: pObserved, stdev: (upper - lower) / N_STDEV };
   }
