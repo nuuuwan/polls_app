68d5c25c12d1c8bc43c9aa267f039a2ce5befe32
nuuuwan <nuuuwan@gmail.com>
2022-05-21 20:18:45 +0530

    Made Error bounds symmetrical

--- src/base/StatisticsXFuture.js
+++ src/base/StatisticsXFuture.js
@@ -5,25 +5,26 @@ export default class StatisticsXFuture {
   static MIN_STATISTICAL_N = 1;
 
   static getErrorBounds(nObserved, npObserved) {
-    const SIGNIFICANCE = 0.05;
+    const CONFIDENCE = 0.95;
     const N_STDEV = 2;
     const pObserved = npObserved / nObserved;
     const Q = 100;
     let [lower, upper] = [1, 1];
-    for (let i in DataStructures.range(0, Q + 1)) {
-      const pActual = i / Q;
-      const cdfForObserved = cdf(npObserved, nObserved, pActual);
 
-      if (lower === 1 && cdfForObserved < 1 - SIGNIFICANCE / 2) {
-        lower = pActual;
-      }
-      if (cdfForObserved >= SIGNIFICANCE / 2) {
-        upper = pActual;
+    for (let i in DataStructures.range(0, Q + 1)) {
+      const pActualLower = i / Q;
+      const cdfForObservedLower = cdf(npObserved, nObserved, pActualLower);
+      const pActualUpper = 1 - i / Q;
+      const cdfForObservedUpper = cdf(npObserved, nObserved, pActualUpper);
+
+      const span = cdfForObservedLower - cdfForObservedUpper;
+      if (span < CONFIDENCE) {
+        lower = pActualLower;
+        upper = pActualUpper;
+        break;
       }
     }
-
     lower = Math.min(upper, lower);
-
     return { lower, upper, p: pObserved, stdev: (upper - lower) / N_STDEV };
   }
 }
