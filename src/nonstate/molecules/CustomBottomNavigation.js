2f2f9350d4de51aed751b9d12eccf8141f35d7ff
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 10:01:43 +0530

    Removed BottomNavigationActions

--- src/nonstate/molecules/CustomBottomNavigation.js
+++ src/nonstate/molecules/CustomBottomNavigation.js
@@ -1,58 +1,15 @@
 import React from "react";
 
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import Tooltip from "@mui/material/Tooltip";
 import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
-export default function CustomBottomNavigation({
-  onClickPrevious,
-  onClickNext,
-}) {
-  const navigationItems = [
-    {
-      name: "Previous Poll",
-      details: "Go to previous poll",
-      Icon: NavigateBeforeIcon,
-      onClick: onClickPrevious,
-    },
-    {
-      name: "Next Poll",
-      details: "Go to next poll",
-      Icon: NavigateNextIcon,
-      onClick: onClickNext,
-    },
-  ];
-
+export default function CustomBottomNavigation() {
   return (
     <Paper
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
-      <BottomNavigation showLabels>
-        {navigationItems.map(function (item, i) {
-          const key = "data-" + i;
-          const Icon = item.Icon;
-          const onClick = item.onClick;
-          return (
-            <Tooltip
-              key={key}
-              title={
-                <Typography variant="subtitle1">{item.details}</Typography>
-              }
-            >
-              <BottomNavigationAction
-                label={item.name}
-                icon={<Icon />}
-                onClick={onClick}
-              />
-            </Tooltip>
-          );
-        })}
-      </BottomNavigation>
+      <BottomNavigation showLabels></BottomNavigation>
     </Paper>
   );
 }
