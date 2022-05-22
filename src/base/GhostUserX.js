51d8bfc0f533f4dedd229a93df42f04d025756c4
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 09:27:55 +0530

    Removed USE_RANDOM_SALT; Changed cache keys

--- src/base/GhostUserX.js
+++ src/base/GhostUserX.js
@@ -3,25 +3,22 @@ import Cache from "./Cache";
 import HashX from "./HashX";
 
 const URL_GEOLOCATION_DB = "https://geolocation-db.com/json/";
-const USE_RANDOM_SALT = true;
-const ID_SALT = "adb034fad7a9f45ebc20249cc85eae96";
-const CACHE_KEY_INFO = "polls_app.GhostUserX.info";
+const INFO_HASH_SALT = "adb034fad7a9f45ebc20249cc85eae96";
 
 export default class GhostUserX {
-  static async getInfoNoCache() {
+  static async getGeoLocationInfo() {
     const res = await axios.get(URL_GEOLOCATION_DB);
     const data = res.data;
     const countryCode = data.country_code;
     const countryName = data.country_name;
     const ipV4 = data.IPv4;
     const latLng = [data.latitude, data.longitude];
-    const randomValue = USE_RANDOM_SALT ? Math.random() : ID_SALT;
-    const infoHash = HashX.md5({ countryCode, latLng, ipV4, randomValue });
+    const infoHash = HashX.md5({ countryCode, latLng, ipV4, INFO_HASH_SALT });
     const userID = infoHash;
     return { countryCode, countryName, latLng, ipV4, infoHash, userID };
   }
 
   static async getInfo() {
-    return await Cache.get(CACHE_KEY_INFO, GhostUserX.getInfoNoCache);
+    return await Cache.get('GhostUserX.getInfo', GhostUserX.getGeoLocationInfo);
   }
 }
