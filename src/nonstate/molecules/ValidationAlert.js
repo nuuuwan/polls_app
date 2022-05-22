0e68e15d362a0607325d3189b9c4950d83e674f5
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 16:31:48 +0530

    Deleted unused ValidationAlert

--- src/nonstate/molecules/ValidationAlert.js
+++
@@ -1,6 +0,0 @@
-import Alert from "@mui/material/Alert";
-
-export default function ValidationAlert({ isInvalid, children }) {
-  const severity = isInvalid ? "error" : "success";
-  return <Alert severity={severity}>{children}</Alert>;
-}
