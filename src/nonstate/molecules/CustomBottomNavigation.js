b1453aa3969c038f4901a5e71f6e422080b0f152
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 12:15:44 +0530

    Added "Add New Poll" to CustomBottomNavigation

--- src/nonstate/molecules/CustomBottomNavigation.js
+++ src/nonstate/molecules/CustomBottomNavigation.js
@@ -2,14 +2,22 @@ import React from "react";
 
 import Paper from "@mui/material/Paper";
 import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import AddIcon from "@mui/icons-material/Add";
 
-export default function CustomBottomNavigation() {
+export default function CustomBottomNavigation({ onClickNewPoll }) {
   return (
     <Paper
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
-      <BottomNavigation showLabels></BottomNavigation>
+      <BottomNavigation showLabels>
+        <BottomNavigationAction
+          label="Add New Poll"
+          icon={<AddIcon />}
+          onClick={onClickNewPoll}
+        />
+      </BottomNavigation>
     </Paper>
   );
 }
