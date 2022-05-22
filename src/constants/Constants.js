b53027dc6853fdd3da993d2b2fe466909f1951c1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 14:54:52 +0530

    Added Icons

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,10 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import HelpIconOriginal from "@mui/icons-material/Help";
+import BallotIcon from "@mui/icons-material/Ballot";
+
 export const DATE_LAST_UPDATE = "2022-05-22";
 export const URL_GITHUB_REPO_POLLS_APP = "https://github.com/nuuuwan/polls_app";
+
+export const PollIcon = BallotIcon;
+export const HelpIcon = HelpIconOriginal;
+export const CodeIcon = GitHubIcon;
