22252cae0ad29ed0932c9f1450f14a9983c4f20f
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 14:55:06 +0530

    Added HelpIcon

--- src/nonstate/molecules/FAQ.js
+++ src/nonstate/molecules/FAQ.js
@@ -1,12 +1,19 @@
 import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+
+import { HelpIcon } from "../../constants/Constants.js";
 
 export default function FAQ({ faq, iFaq }) {
   return (
     <Paper elevation={1} sx={{ margin: 1, padding: 1 }}>
-      <Typography variant="h5" sx={{ marginBottom: 1 }}>
-        {faq.question}
-      </Typography>
+      <Stack direction="row" spacing={1} sx={{ alignItems: "center" }}>
+        <HelpIcon />
+        <Typography variant="h5" sx={{ marginBottom: 1 }}>
+          {faq.question}
+        </Typography>
+      </Stack>
+
       {faq.answerParagraphs.map(function (answer, iAnswer) {
         const fontWeight = iAnswer === 0 ? "bold" : "normal";
         return (
