3973adacb4a587ad115efb1ff225f66c29dcc7ff
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-22 16:40:31 +0530

    Removed icon. Changed style

--- src/nonstate/molecules/FAQ.js
+++ src/nonstate/molecules/FAQ.js
@@ -2,17 +2,12 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
-import { HelpIcon } from "../../constants/Constants.js";
-
 export default function FAQ({ faq, iFaq }) {
   return (
-    <Paper elevation={1} sx={{ margin: 1, padding: 1 }}>
-      <Stack direction="row" spacing={1} sx={{ alignItems: "center" }}>
-        <HelpIcon />
-        <Typography variant="h5" sx={{ marginBottom: 1 }}>
-          {faq.question}
-        </Typography>
-      </Stack>
+    <Paper elevation={0} sx={{ margin: 2, padding: 2 }}>
+      <Typography variant="h5" sx={{ marginBottom: 2 }}>
+        {faq.question}
+      </Typography>
 
       {faq.answerParagraphs.map(function (answer, iAnswer) {
         const fontWeight = iAnswer === 0 ? "bold" : "normal";
