4d15d494aba5dffcc27d9b9ca8144d6d36760b46
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 18:22:19 +0530

    Added "your Vote" to PollTitle

--- src/nonstate/atoms/PollTitle.js
+++ src/nonstate/atoms/PollTitle.js
@@ -4,17 +4,23 @@ import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 
 import PollStatisticsView from "../../nonstate/molecules/PollStatisticsView";
-
 import { MathX } from "@nuuuwan/utils-js-dev";
 import { PublicIcon, UnlistedIcon } from "../../constants/CommonIcons";
 import AlignCenter from "../../nonstate/atoms/AlignCenter";
+import Condition from "../../nonstate/atoms/Condition";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
 
 export default function PollTitle({ pollExtended }) {
   const VisibilityIcon =
     pollExtended.visibility === "public" ? PublicIcon : UnlistedIcon;
 
+  const isUserAnswer = pollExtended.userAnswer && pollExtended.userAnswer !== "";
+
   const theme = useTheme();
-  const color = theme.palette.primary.main;
+  const color = isUserAnswer
+    ? theme.palette.secondary.main
+    : theme.palette.primary.main;
 
   const totalCount = MathX.sum(Object.values(pollExtended.answerToCount));
 
@@ -36,6 +42,15 @@ export default function PollTitle({ pollExtended }) {
           {pollExtended.visibility}
         </Typography>
       </AlignCenter>
+
+      <Condition condition={isUserAnswer}>
+        <AlignCenter>
+          <CheckCircleIcon sx={{ fontSize: "small", color: color }} />
+          <Typography style={{ fontSize: "small", color: color }}>
+            You voted "{pollExtended.userAnswer}"
+          </Typography>
+        </AlignCenter>
+      </Condition>
     </Stack>
   );
 }
