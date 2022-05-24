ee42f37e88bd5c8e272313e58b5b4bbdf9d0e10e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 18:15:53 +0530

    Changed Title font; Added Divider; Removed PollIcon

--- src/nonstate/atoms/PollTitle.js
+++ src/nonstate/atoms/PollTitle.js
@@ -1,41 +1,41 @@
+import { useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+
 import PollStatisticsView from "../../nonstate/molecules/PollStatisticsView";
 
-import { TimeX, MathX } from "@nuuuwan/utils-js-dev";
-import {
-  PollIcon,
-  PublicIcon,
-  UnlistedIcon,
-} from "../../constants/CommonIcons";
+import { MathX } from "@nuuuwan/utils-js-dev";
+import { PublicIcon, UnlistedIcon } from "../../constants/CommonIcons";
 import AlignCenter from "../../nonstate/atoms/AlignCenter";
 
 export default function PollTitle({ pollExtended }) {
-  const VisibilityIcon = pollExtended.visibility === "public" ? PublicIcon : UnlistedIcon;
+  const VisibilityIcon =
+    pollExtended.visibility === "public" ? PublicIcon : UnlistedIcon;
+
+  const theme = useTheme();
+  const color = theme.palette.primary.main;
 
   const totalCount = MathX.sum(Object.values(pollExtended.answerToCount));
 
   return (
     <Stack>
+      <Typography variant="h6" color={color}>{pollExtended.question}</Typography>
+
       <AlignCenter>
+        <PollStatisticsView
+          answerList={pollExtended.answerList}
+          totalCount={totalCount}
+          answerToCount={pollExtended.answerToCount}
+        />
+
+        <Divider orientation="vertical" variant="middle" flexItem />
+
         <VisibilityIcon sx={{ fontSize: "small", color: "gray" }} />
         <Typography sx={{ fontSize: "small", color: "gray" }}>
           {pollExtended.visibility}
         </Typography>
       </AlignCenter>
-
-      <AlignCenter>
-        <PollIcon />
-        <Typography variant="subtitle1">
-          {pollExtended.question}
-        </Typography>
-      </AlignCenter>
-
-      <PollStatisticsView
-        answerList={pollExtended.answerList}
-        totalCount={totalCount}
-        answerToCount={pollExtended.answerToCount}
-      />
     </Stack>
   );
 }
