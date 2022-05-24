a3a2aa57d4d564f4a56f04944991b1ce6e60ba7a
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 16:39:17 +0530

    Added Public and Unlisted Icons

--- src/constants/CommonIcons.js
+++ src/constants/CommonIcons.js
@@ -2,8 +2,13 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import HelpIconOriginal from "@mui/icons-material/Help";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import PersonIcon from "@mui/icons-material/Person";
+import PeopleIcon from "@mui/icons-material/People";
+import LinkIcon from "@mui/icons-material/Link";
 
 export const PollIcon = HowToVoteIcon;
 export const HelpIcon = HelpIconOriginal;
 export const CodeIcon = GitHubIcon;
 export const UserIcon = PersonIcon;
+
+export const PublicIcon = PeopleIcon;
+export const UnlistedIcon = LinkIcon;
