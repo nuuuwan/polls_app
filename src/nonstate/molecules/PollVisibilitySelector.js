84dee26d9cba9c7e429ec6620cf147bb521346d7
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 14:34:23 +0530

    Added alert message

--- src/nonstate/molecules/PollVisibilitySelector.js
+++ src/nonstate/molecules/PollVisibilitySelector.js
@@ -3,11 +3,37 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import Alert from '@mui/material/Alert';
 
 export default function PollVisibilitySelector({ visibility, onChange }) {
   const onChangeInner = function (e) {
     onChange(e.target.value);
   };
+
+  const alertMessage = (visibility === 'public') ? (
+    <>
+      You have
+      chossen <strong>"public"</strong>,
+      which means that this poll <strong>will</strong> appear
+      when users search for polls
+      and on "Random Public Polls".
+
+      If you don't want this, choose <strong>"unlisted"</strong>.
+    </>
+  ) : (
+    <>
+      You have
+      chossen <strong>"unlisted"</strong>,
+      which means that this poll <strong>will not</strong> appear
+      when users search for polls
+      and on "Random Public Polls".
+
+      The only way to share this poll is with a link.
+      Anyone with a link to this poll can see it.
+
+      If you don't want this, choose <strong>"public"</strong>.
+    </>
+  );
 
   return (
     <FormControl>
@@ -24,6 +50,9 @@ export default function PollVisibilitySelector({ visibility, onChange }) {
           label="Unlisted Poll"
         />
       </RadioGroup>
+      <Alert severity="info">
+        {alertMessage}
+      </Alert>
     </FormControl>
   );
 }
