aea6e3aac4ac8098d4f15f0c46fa177df28648ea
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 18:32:50 +0530

    Remove static constructor

--- src/core/AudioX.js
+++ src/core/AudioX.js
@@ -7,13 +7,10 @@ const URL_CLICK = URL_BASE + "/tabla-click.mp3";
 const URL_VOTE = URL_BASE + "/tabla-vote.mp3";
 
 export default class AudioX {
-  static tracks;
-  static {
-    this.tracks = {
-      click: new Audio(URL_CLICK),
-      vote: new Audio(URL_VOTE),
-    };
-  }
+  static tracks=  {
+    click: new Audio(URL_CLICK),
+    vote: new Audio(URL_VOTE),
+  };
 
   static pauseAll() {
     Object.values(AudioX.tracks).forEach(function (track) {
