6bd8bfc9f9eedca0805f10002491df4f7ddbd7be
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 13:57:27 +0530

    Added new attribute visibility

--- src/core/Poll.js
+++ src/core/Poll.js
@@ -1,8 +1,9 @@
 export default class Poll {
-  constructor(pollID, question, answerList) {
+  constructor(pollID, question, answerList, visibility) {
     this.pollID = pollID;
     this.question = question;
     this.answerList = answerList;
+    this.visibility = visibility;
   }
 
   static toDict(poll) {
@@ -10,10 +11,16 @@ export default class Poll {
       pollID: poll.pollID,
       question: poll.question,
       answerListJSON: JSON.stringify(poll.answerList),
+      visibility: poll.visibility,
     };
   }
 
   static fromDict(d) {
-    return new Poll(d.pollID, d.question, JSON.parse(d.answerListJSON));
+    return new Poll(
+      d.pollID,
+      d.question,
+      JSON.parse(d.answerListJSON),
+      d.visibility
+    );
   }
 }
