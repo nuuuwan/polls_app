25a4876aebf23a94a08253d21409684e6ec1d3ec
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 11:13:32 +0530

    Updated LAST_UPDATE = "2022-05-24";

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -3,7 +3,7 @@ import HelpIconOriginal from "@mui/icons-material/Help";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import PersonIcon from "@mui/icons-material/Person";
 
-export const DATE_LAST_UPDATE = "2022-05-22";
+export const DATE_LAST_UPDATE = "2022-05-24";
 export const URL_GITHUB_REPO_POLLS_APP = "https://github.com/nuuuwan/polls_app";
 
 export const PollIcon = HowToVoteIcon;
