7b23c244a50d9839626c30836b0ff1e9f3a013ee
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-24 18:32:42 +0530

    Delete

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", async () => {
-  render(<App />);
-  const linkElement = await screen.findByText("Polls App");
-  expect(linkElement).toBeInTheDocument();
-});
