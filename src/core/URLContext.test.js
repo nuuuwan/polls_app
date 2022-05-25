af166010e58bb9bf831494b500cedc08e274ea71
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 14:52:06 +0530

    Added tests

---
+++ src/core/URLContext.test.js
@@ -0,0 +1,11 @@
+import URLContext from "./URLContext";
+
+import PollPage from "../stateful/pages/PollPage";
+import HelpPage from "../stateful/pages/HelpPage";
+import UserPage from "../stateful/pages/UserPage";
+
+test("getPageName", () => {
+  expect(URLContext.getPageName(PollPage)).toBe('poll');
+  expect(URLContext.getPageName(HelpPage)).toBe('help');
+  expect(URLContext.getPageName(UserPage)).toBe('user');
+});
