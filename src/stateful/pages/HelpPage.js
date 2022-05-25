f27897643d37747c03540c9ae7bac1d8698e1a00
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 08:32:00 +0530

    Removed unused

--- src/stateful/pages/HelpPage.js
+++ src/stateful/pages/HelpPage.js
@@ -2,9 +2,7 @@ import { Component } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-import { URL_GITHUB_REPO_POLLS_APP } from "../../constants/Constants";
 import { FAQList } from "../../constants/FAQ";
-import SimpleLink from "../../nonstate/atoms/SimpleLink";
 import FAQ from "../../nonstate/molecules/FAQ";
 import { HelpIcon } from "../../constants/CommonIcons.js";
 import AlignCenter from "../../nonstate/atoms/AlignCenter";
