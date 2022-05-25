b40f1ae42937e3b474635136861e61aeb35ff7e6
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 23:10:35 +0530

    Moved STYLE into component

--- src/nonstate/atoms/VoteButton.js
+++ src/nonstate/atoms/VoteButton.js
@@ -2,15 +2,13 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 
-const STYLE_BUTTON = {
-  maxWidth: 150,
-};
-
 export default function VoteButton({ onClick, disabled }) {
   return (
     <Stack direction="row" justifyContent="right" spacing={1} sx={{ m: 1 }}>
       <Button
-        sx={STYLE_BUTTON}
+        sx={{
+          maxWidth: 150,
+        }}
         startIcon={<HowToVoteIcon />}
         variant="contained"
         onClick={onClick}
