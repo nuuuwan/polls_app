d2fd764099093b1d4154a641b654f1ebcf013994
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 12:43:03 +0530

    Added previously failed tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,20 @@ jest.setTimeout(120_000);
 test("Polls Page", async () => {
   render(<App />);
 
+  // AppBar
   await waitFor(() => screen.findByText("Polls App"), { timeout: 60_000 });
 
-  const appBarTitle = screen.getByText("Polls App");
-  expect(appBarTitle).toBeInTheDocument();
+  expect(screen.getByText("Polls App")).toBeInTheDocument();
+
+  // PollPage
+  await waitFor(() => screen.findByText("More Public Polls..."), {
+    timeout: 60_000,
+  });
+
+  expect(screen.getByText("More Public Polls...")).toBeInTheDocument();
+
+  expect(screen.getByText("Copy")).toBeInTheDocument();
+  expect(screen.getByText("Share")).toBeInTheDocument();
+  expect(screen.getByText("Add New")).toBeInTheDocument();
+  expect(screen.getByText("Random")).toBeInTheDocument();
 });
