7068ded7e9172571a5cc6fb6f17b96cefee4f299
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 09:22:10 +0530

    Show divider

--- src/nonstate/molecules/FAQ.js
+++ src/nonstate/molecules/FAQ.js
@@ -1,9 +1,21 @@
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 import FAQParagraph from "./FAQParagraph";
 
 export default function FAQ({ faq, iFaq }) {
+  if (faq.question === "divider") {
+    return (
+      <Typography
+        variant="h4"
+        sx={{ textAlign: "center", color: "gray", marginBottom: 1 }}
+      >
+        ...
+      </Typography>
+    );
+  }
+
   return (
     <Paper elevation={0} sx={{ padding: 1 }}>
       <Typography variant="h5" sx={{ marginBottom: 1 }}>
