2836a02c610f9b6f14198b3247a9ac306eb918b9
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 14:52:12 +0530

    Lint Only

--- src/base/StatisticsXFuture.test.js
+++ src/base/StatisticsXFuture.test.js
@@ -8,6 +8,7 @@ test("getErrorBounds", () => {
   expect(bounds.lower).toBeCloseTo(0.21, 6);
   expect(bounds.upper).toBeCloseTo(0.79, 6);
   expect(bounds.p).toBeCloseTo(0.5, 6);
+  
   expect(bounds.stdev).toBeCloseTo(0.145, 6);
 
 });
