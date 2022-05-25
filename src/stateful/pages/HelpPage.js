ef7835631cbc9fd0749c51d80aabc513f7f96488
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 08:00:41 +0530

    Use FAQList from FAQ

--- src/stateful/pages/HelpPage.js
+++ src/stateful/pages/HelpPage.js
@@ -3,30 +3,11 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 import { URL_GITHUB_REPO_POLLS_APP } from "../../constants/Constants";
+import { FAQList } from "../../constants/FAQ";
 import SimpleLink from "../../nonstate/atoms/SimpleLink";
 import FAQ from "../../nonstate/molecules/FAQ";
 import { HelpIcon } from "../../constants/CommonIcons.js";
 import AlignCenter from "../../nonstate/atoms/AlignCenter";
-
-const FAQS = [
-  {
-    question: "Where can I find the code for this App?",
-    answerParagraphs: [
-      <>
-        {"You can find the code on "}
-        <SimpleLink href={URL_GITHUB_REPO_POLLS_APP} label="GitHub" />.
-      </>,
-      "Feel free to fork the code, report issues, or ask questions.",
-    ],
-  },
-  {
-    question: "How many times can I vote in a single poll?",
-    answerParagraphs: [
-      "You can vote any number of times.",
-      "Your most recent vote will be counted",
-    ],
-  },
-];
 
 export default class HelpPage extends Component {
   render() {
@@ -36,7 +17,7 @@ export default class HelpPage extends Component {
           <HelpIcon />
           <Typography variant="h4">FAQs</Typography>
         </AlignCenter>
-        {FAQS.map(function (faq, iFaq) {
+        {FAQList.map(function (faq, iFaq) {
           return <FAQ key={"faq-" + iFaq} faq={faq} iFaq={iFaq} />;
         })}
       </Box>
