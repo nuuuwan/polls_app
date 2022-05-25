350c01cc57805f50ac0303078127e1e156820251
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 11:55:43 +0530

    Added test for getPollIDs

--- src/core/PollsAppServer.test.js
+++ src/core/PollsAppServer.test.js
@@ -3,3 +3,8 @@ import PollsAppServer from "./PollsAppServer";
 test("getCacheKey", () => {
   expect(PollsAppServer.getCacheKey([1, 2, 3])).toBe("cacheKey:1:2:3");
 });
+
+test("getPollIDs", async () => {
+  const pollIDs = await PollsAppServer.getPollIDs();
+  expect(pollIDs.length).toBeGreaterThanOrEqual(5);
+});
