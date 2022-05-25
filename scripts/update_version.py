6cbcc75da8f3430b20e4add1a7d04f3ea8e54345
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 11:10:28 +0530

    Added linting

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -18,6 +18,14 @@ def update_version():
     return last_update
 
 
+def lint():
+    for cmd_prefix in [
+        'npx prettier --write --loglevel warn',
+        'eslint --fix --ext',
+    ]:
+        os.system(f'{cmd_prefix} {VERSION_FILE_NAME}')
+
+
 def git_update(last_update):
     os.system(f'git add {VERSION_FILE_NAME}')
     os.system(f'git commit -m "[update_version] Updated to {last_update}"')
@@ -25,4 +33,5 @@ def git_update(last_update):
 
 if __name__ == '__main__':
     last_update = update_version()
+    lint()
     git_update(last_update)
