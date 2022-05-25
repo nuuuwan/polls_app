6893c49162ea68183520375bcea1f1b92c4bec00
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 23:07:31 +0530

    Moved STYLE into component

--- src/nonstate/atoms/VersionWidget.js
+++ src/nonstate/atoms/VersionWidget.js
@@ -2,12 +2,16 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import { VERSION } from "../../constants/Version";
 
-const STYLE_VERSION = {
-  fontSize: "50%",
-  color: "gray",
-  textAlign: "center",
-};
-
 export default function VersionWidget() {
-  return <Typography sx={STYLE_VERSION}>Last Updated {VERSION}</Typography>;
+  return (
+    <Typography
+      sx={{
+        fontSize: "50%",
+        color: "gray",
+        textAlign: "center",
+      }}
+    >
+      Last Updated {VERSION}
+    </Typography>
+  );
 }
