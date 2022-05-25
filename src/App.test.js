36fb21acd22d8fcd00c4356e0218fdd3351403f7
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-25 11:11:07 +0530

    Added tests for BottomBar and PollingDirectory

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", async () => {
+const PARAMS = { timeout: 20_000 };
+
+test("Polls Page", async () => {
   render(<App />);
-  const linkElement = await screen.findByText("Polls App");
-  expect(linkElement).toBeInTheDocument();
+
+  const appBarTitle = await screen.findByText("Polls App", undefined, PARAMS);
+  expect(appBarTitle).toBeInTheDocument();
+
+  const morePublicPollsLabel = await screen.findByText(
+    "More Public Polls...",
+    undefined,
+    PARAMS
+  );
+  expect(morePublicPollsLabel).toBeInTheDocument();
+
+  for (let buttonText of ["Copy", "Share", "Add New", "Random"]) {
+    const button = await screen.findByText(buttonText, undefined, PARAMS);
+    expect(button).toBeInTheDocument();
+  }
 });
