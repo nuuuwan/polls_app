8d3a3f163269b03b4c36f95f00327ecb9d898b02
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-26 11:26:38 +0530

    Removed Circle

--- src/nonstate/atoms/EquilateralPolygon.js
+++ src/nonstate/atoms/EquilateralPolygon.js
@@ -1,15 +1,15 @@
 import { DataStructures } from "@nuuuwan/utils-js-dev";
 
-import Circle from "./Circle";
-
+const THETA0 = -Math.PI / 2;
 function getD(cx, cy, r, n) {
   return (
     DataStructures.range(0, n)
       .map(function (i) {
-        const theta0 = -Math.PI / 2;
-        const theta = theta0 + (Math.PI * 2 * i) / n;
-        const x = parseInt(cx + r * Math.cos(theta));
-        const y = parseInt(cy + r * Math.sin(theta));
+        const theta = THETA0 + (Math.PI * 2 * i) / n;
+        const [x, y] = [
+          cx + r * Math.cos(theta),
+          cy + r * Math.sin(theta),
+        ].map(x => parseInt(x));
         const label = i === 0 ? "M" : "L";
         return label + x + "," + y;
       })
@@ -18,10 +18,6 @@ function getD(cx, cy, r, n) {
 }
 
 export default function EquilateralPolygon({ cx, cy, r, n, color }) {
-  if (n > 4) {
-    return <Circle cx={cx} cy={cy} r={r} color={color} />;
-  }
-
   const d = getD(cx, cy, r, n);
   return (
     <svg>
