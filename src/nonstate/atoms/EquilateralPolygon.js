31d53b46fa20a62ecf6d1f412e90954543f94d9b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-26 11:28:56 +0530

    Lint only

--- src/nonstate/atoms/EquilateralPolygon.js
+++ src/nonstate/atoms/EquilateralPolygon.js
@@ -6,10 +6,9 @@ function getD(cx, cy, r, n) {
     DataStructures.range(0, n)
       .map(function (i) {
         const theta = THETA0 + (Math.PI * 2 * i) / n;
-        const [x, y] = [
-          cx + r * Math.cos(theta),
-          cy + r * Math.sin(theta),
-        ].map(x => parseInt(x));
+        const [x, y] = [cx + r * Math.cos(theta), cy + r * Math.sin(theta)].map(
+          (x) => parseInt(x)
+        );
         const label = i === 0 ? "M" : "L";
         return label + x + "," + y;
       })
