0f7ac73535f99d1d15c5a6973dc8510b722adafe
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-26 13:02:01 +0530

    Renamed Math to MathX to avoid confusion with JS Math class.

--- src/nonview/base/MathX.test.js
+++ src/nonview/base/MathX.test.js
@@ -1,7 +1,7 @@
-import Math from "./Math";
+import MathX from "./MathX";
 
 test("shuffle", async () => {
   const arr = [1, 2, 3, 4];
-  const arrShuffled = Math.randomShuffle(arr);
+  const arrShuffled = MathX.randomShuffle(arr);
   expect(arrShuffled.length).toBe(4);
 });
