e5ac480b8a22029cecf7160cc2aeeb918339d73b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-26 15:37:06 +0530

    User PollAnswerGroup

--- src/view/molecules/PollViewMolecule.js
+++ src/view/molecules/PollViewMolecule.js
@@ -1,12 +1,10 @@
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import RadioGroup from "@mui/material/RadioGroup";
 
-import { MathX } from "@nuuuwan/utils-js-dev";
 import VoteButton from "../../view/atoms/VoteButton";
-import PollAnswer from "../../view/molecules/PollAnswer";
 import ValidationBox from "../../view/molecules/ValidationBox";
 import PollTitle from "../../view/molecules/PollTitle";
+import PollAnswerGroup from "../../view/molecules/PollAnswerGroup";
 
 const STYLE = {
   margin: 0,
@@ -31,13 +29,10 @@ export default function PollViewMolecule({
   };
 
   const userAnswer = pollExtended.userAnswer;
-  const hasUserVote = userAnswer && userAnswer !== ANSWER_NONE;
   const hasSelectedOption = selectedAnswer && selectedAnswer !== ANSWER_NONE;
   const isSelectionUserAnswer =
     hasSelectedOption && selectedAnswer === userAnswer;
   const isVoteButtonDisabled = !hasSelectedOption || isSelectionUserAnswer;
-  const showStatistics = hasUserVote;
-  const totalCount = MathX.sum(Object.values(pollExtended.answerToCount));
 
   return (
     <Stack
@@ -47,43 +42,14 @@ export default function PollViewMolecule({
     >
       <PollTitle pollExtended={pollExtended} />
 
-      <ValidationBox
-        isValid={hasSelectedOption && !isSelectionUserAnswer}
-        alertIfValid={
-          <>
-            Looks good. Now, click <strong>Vote</strong> to submit your vote.
-          </>
-        }
-        alertIfInvalid={
-          isSelectionUserAnswer ? (
-            ""
-          ) : (
-            <>
-              Select <strong>one</strong> answer.
-            </>
-          )
-        }
-      >
-        <Box sx={{ m: 0, marginLeft: 1 }}>
-          <RadioGroup value={selectedAnswer} onChange={onChange}>
-            {pollExtended.answerList.map(function (answer, iAnswer) {
-              const answerVotes = pollExtended.answerToCount[answer]
-                ? pollExtended.answerToCount[answer]
-                : 0;
-              return (
-                <PollAnswer
-                  key={"poll-answer-" + iAnswer}
-                  answer={answer}
-                  answerVotes={answerVotes}
-                  totalCount={totalCount}
-                  showStatistics={showStatistics}
-                  userAnswer={userAnswer}
-                />
-              );
-            })}
-          </RadioGroup>
-        </Box>
-      </ValidationBox>
+      <PollAnswerGroup
+        pollExtended={pollExtended}
+        selectedAnswer={selectedAnswer}
+        hasSubmittedVote={hasSubmittedVote}
+        setSelectedAnswer={setSelectedAnswer}
+        onClickVoteInner={onClickVoteInner}
+        onChange={onChange}
+      />
 
       <ValidationBox
         isValid={hasSubmittedVote}
