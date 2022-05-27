45ff2f68da0c5b8e2fbc41f84ae951c9bd2ecf6e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-27 09:25:33 +0530

    Use tests/Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,68 +1,38 @@
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
-import App from "./App";
-
-jest.setTimeout(120_000);
-const PARAMS_TIMEOUT = { timeout: 20_000 };
-
-jest
-  .spyOn(window.HTMLMediaElement.prototype, "play")
-  .mockImplementation(() => {});
-
-async function defaultPageLoad() {
-  render(<App />);
-  await waitFor(() => screen.findByText("Polls App"), PARAMS_TIMEOUT);
-  expect(screen.getByText("Polls App")).toBeInTheDocument();
-}
-
-async function clickOnMenu() {
-  const button = screen.getByRole("button", {
-    name: "CustomAppBarMenu.button",
-  });
-  fireEvent(button, new MouseEvent("click", { bubbles: true }));
-}
+import {
+  screenFindByText,
+  click,
+  defaultAppLoad,
+  clickOnMenu,
+} from "./view/tests/Tests";
 
 test("Polls Page", async () => {
-  await defaultPageLoad();
+  await defaultAppLoad();
 });
 
 test("Add New Poll", async () => {
-  await defaultPageLoad();
-
-  await waitFor(() => screen.findByText("Add New"), PARAMS_TIMEOUT);
-  const button = screen.getByText("Add New");
-  fireEvent(button, new MouseEvent("click", { bubbles: true }));
-
-  // Validate Add New Poll Drawer
-  expect(screen.getByText("Question")).toBeInTheDocument();
-  await waitFor(() => screen.findByText("Question"), PARAMS_TIMEOUT);
-  expect(screen.getByText("Question")).toBeInTheDocument();
+  await defaultAppLoad();
+  const button = await screenFindByText("Add New");
+  click(button);
+  await screenFindByText("Question");
 });
 
 test("Help Page", async () => {
-  await defaultPageLoad();
-  await clickOnMenu();
-
-  // Click on Help
-  await waitFor(() => screen.findByText("Help & FAQs"), PARAMS_TIMEOUT);
-  const buttonHelp = screen.getByText("Help & FAQs");
-  fireEvent(buttonHelp, new MouseEvent("click", { bubbles: true }));
-
-  // Validate HelpPage
-  await waitFor(() => screen.findByText("FAQs"), PARAMS_TIMEOUT);
-  expect(screen.getByText("FAQs")).toBeInTheDocument();
+  await clickOnMenu("Help & FAQs");
+  await screenFindByText("FAQs");
 });
 
 test("User Page", async () => {
-  await defaultPageLoad();
-  await clickOnMenu();
+  await clickOnMenu("User");
+  await screenFindByText("UserID");
+});
 
-  // Click on User
-  await waitFor(() => screen.findByText("User"), PARAMS_TIMEOUT);
-  const buttonHelp = screen.getByText("User");
-  fireEvent(buttonHelp, new MouseEvent("click", { bubbles: true }));
-
-  // Validate UserPage
-  await waitFor(() => screen.findByText("UserID"), PARAMS_TIMEOUT);
-  expect(screen.getByText("UserID")).toBeInTheDocument();
+test("User Avatar Button", async () => {
+  await defaultAppLoad();
+  const buttonTop = screen.getByRole("button", {
+    name: "CustomAppBar.avatar.button",
+  });
+  click(buttonTop);
+  await screenFindByText("UserID");
 });
