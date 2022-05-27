2bf7228e7a32126c7fd552f952d87849c977d35e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-27 11:34:42 +0530

    Added TestData

---
+++ src/nonview/tests/TestData.js
@@ -0,0 +1,17 @@
+export const testGeoLocationInfo = {
+  countryCode: "LK",
+  countryName: "Sri Lanka",
+  latLng: [1, 1],
+  ipV4: "1.1.1.1",
+  infoHash: "11111111111111111111111111111111",
+  userID: "11111111111111111111111111111111",
+};
+
+export const testPollExtended = {
+  pollID: "test-poll-id-0",
+  question: "Does this test question have an answer?",
+  answerListJSON: JSON.stringify(["yes", "no"]),
+  visibility: "public",
+  answerToCount: { yes: 123, no: 54 },
+  userAnswer: "yes",
+};
