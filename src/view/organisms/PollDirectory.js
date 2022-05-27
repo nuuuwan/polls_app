72f272c4cee148d1c27d0d8ef360afe1acfcf165
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-27 08:38:08 +0530

    Removed import * as React from "react";

--- src/view/organisms/PollDirectory.js
+++ src/view/organisms/PollDirectory.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import * as React from "react";
 
 import CircularProgress from "@mui/material/CircularProgress";
 
--- src/view/pages/UserPage.js
+++ src/view/pages/UserPage.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import * as React from "react";
 import ReactCountryFlag from "react-country-flag";
 
 import Alert from "@mui/material/Alert";
