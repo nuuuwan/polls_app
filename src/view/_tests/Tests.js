42ccb34e1cb25bbe62efcea0f9561538375a90e4
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-27 12:05:23 +0530

    Fixed act Bug

--- src/view/_tests/Tests.js
+++ src/view/_tests/Tests.js
@@ -1,13 +1,19 @@
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  fireEvent,
+  act,
+} from "@testing-library/react";
 
 import { testPollExtended } from "../../nonview/tests/TestData";
 
 import App from "../../App";
 
-const PARAMS_TIMEOUT = { timeout: 5_000 };
+const PARAMS_TIMEOUT = { timeout: 3_000 };
 const PARAMS_EVENT = { bubbles: true };
 
-jest.setTimeout(60_000);
+jest.setTimeout(10_000);
 jest
   .spyOn(window.HTMLMediaElement.prototype, "play")
   .mockImplementation(() => {});
@@ -17,11 +23,15 @@ export async function screenFindByText(text) {
 }
 
 export function click(element) {
-  fireEvent(element, new MouseEvent("click", PARAMS_EVENT));
+  act(() => {
+    fireEvent(element, new MouseEvent("click", PARAMS_EVENT));
+  });
 }
 
 export async function defaultAppLoad() {
-  render(<App />);
+  await act(async () => {
+    render(<App />);
+  });
   await screenFindByText("Polls App");
   screenFindByText(testPollExtended.question);
 }
