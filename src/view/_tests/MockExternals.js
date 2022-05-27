a21d95f890dd4dd79c55f21afbf72288d170d86f
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-27 11:35:50 +0530

    MockExternals

---
+++ src/view/_tests/MockExternals.js
@@ -0,0 +1,51 @@
+import AWSDynamoDB from "../../nonview/base/AWSDynamoDB";
+import Cache from "../../nonview/base/Cache";
+import GhostUser from "../../nonview/base/GhostUser";
+import {
+  testGeoLocationInfo,
+  testPollExtended,
+} from "../../nonview/tests/TestData";
+
+export function mock() {
+  jest
+    .spyOn(Cache, "get")
+    .mockImplementation(async function (cacheKey, asyncFallback) {
+      return await asyncFallback();
+    });
+
+  jest
+    .spyOn(GhostUser, "getGeoLocationInfo")
+    .mockImplementation(async function () {
+      return testGeoLocationInfo;
+    });
+
+  jest
+    .spyOn(AWSDynamoDB, "generic")
+    .mockImplementation(async function (payload) {
+      if (payload.cmd === "multiget-ids-polls") {
+        return [
+          {
+            pollID: testPollExtended.pollID,
+          },
+        ];
+      }
+
+      if (payload.cmd === "get-poll-extended") {
+        return testPollExtended;
+      }
+
+      if (payload.cmd === "put-poll") {
+        return payload.d;
+      }
+
+      if (payload.cmd === "put-poll-result") {
+        return payload.d;
+      }
+
+      return undefined;
+    });
+}
+
+export function unmock() {
+  jest.restoreAllMocks();
+}
