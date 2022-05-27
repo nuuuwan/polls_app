a716f764e76429bd3a3468d527ab1a45898a38d3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-27 11:36:00 +0530

    Run with mocks

--- src/App.AppLoad.test.js
+++ src/App.AppLoad.test.js
@@ -1,4 +1,8 @@
-import { defaultAppLoad } from "./view/tests/Tests";
+import { mock, unmock } from "./view/_tests/MockExternals";
+import { defaultAppLoad } from "./view/_tests/Tests";
+
+beforeEach(mock);
+afterAll(unmock);
 
 test("Polls Page", async () => {
   await defaultAppLoad();
