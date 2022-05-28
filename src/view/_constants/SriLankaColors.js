fe4754e6724a489ff3e239c14765bcd12d13e1a1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 17:54:00 +0530

    Updated colors from https://www.britannica.com/topic/flag-of-Sri-Lanka

--- src/view/_constants/SriLankaColors.js
+++ src/view/_constants/SriLankaColors.js
@@ -1,12 +1,8 @@
-function rgb(r, g, b) {
-  return `rgb(${r},${g},${b})`;
-}
-
 const SriLankaColors = {
-  Buddhist: rgb(255, 190, 41),
-  Sinhala: rgb(141, 21, 58),
-  Tamil: rgb(235, 116, 0),
-  Muslim: rgb(0, 83, 78),
+  Buddhist: "#FFBD29",
+  Sinhala: "#8C1539",
+  Tamil: "#EA7500",
+  Muslim: "#01544E",
 };
 
 export default SriLankaColors;
