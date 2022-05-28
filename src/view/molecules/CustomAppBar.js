69c3b35515386f7a624d8902c0fe3afcc0e09c1a
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 10:38:31 +0530

    Removed onClickUser; unused imports

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -1,15 +1,9 @@
 import React from "react";
 
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-
-import AudioX from "../../nonview/core/AudioX";
 
 import CustomAppBarMenu from "./CustomAppBarMenu";
-import IDAvatar from "../molecules/IDAvatar";
-import UserPage from "../../view/pages/UserPage";
 
 const STYLE = {
   position: "fixed",
@@ -18,10 +12,6 @@ const STYLE = {
 };
 
 export default function CustomAppBar({ onSelectPage, geoInfo, Page }) {
-  const onClickUser = async function () {
-    onSelectPage(UserPage);
-    await AudioX.playClick();
-  };
   return (
     <Grid container sx={STYLE} justifyContent="flex-end">
       <Toolbar variant="dense">
