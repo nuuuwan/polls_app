6acb137c673217db2f161d8c0118a0477b7ef30b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 10:32:00 +0530

    Style only

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import AppBar from "@mui/material/AppBar";
+import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -12,12 +12,9 @@ import IDAvatar from "../molecules/IDAvatar";
 import UserPage from "../../view/pages/UserPage";
 
 const STYLE = {
-  backgroundColor: "lightgray",
   position: "fixed",
   top: 0,
   left: 0,
-  right: 0,
-  height: 50,
 };
 
 export default function CustomAppBar({ onSelectPage, geoInfo, Page }) {
@@ -26,22 +23,10 @@ export default function CustomAppBar({ onSelectPage, geoInfo, Page }) {
     await AudioX.playClick();
   };
   return (
-    <AppBar sx={STYLE}>
+    <Grid container sx={STYLE} justifyContent="flex-end">
       <Toolbar variant="dense">
         <CustomAppBarMenu onSelectPage={onSelectPage} Page={Page} />
-
-        <Typography component="div" sx={{ flexGrow: 1 }}>
-          {"Polls App"}
-        </Typography>
-
-        <IconButton
-          sx={{ p: 0 }}
-          onClick={onClickUser}
-          aria-label="CustomAppBar.avatar.button"
-        >
-          <IDAvatar id={geoInfo.userID} size={40} />
-        </IconButton>
       </Toolbar>
-    </AppBar>
+    </Grid>
   );
 }
