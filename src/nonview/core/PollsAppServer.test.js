ff6ded31787df0812c6996642aa08a81bd9584d6
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 17:49:36 +0530

    Unittest for getPollExtended

--- src/nonview/core/PollsAppServer.test.js
+++ src/nonview/core/PollsAppServer.test.js
@@ -8,3 +8,12 @@ test("getPollIDs", async () => {
   const pollIDs = await PollsAppServer.getPollIDs();
   expect(pollIDs.length).toBeGreaterThanOrEqual(5);
 });
+
+test("getPollExtended", async () => {
+  const pollIDs = await PollsAppServer.getPollIDs();
+  const pollID = pollIDs[0];
+  const userID = "123456789abcdef0123456789abcdef0";
+  const pollExtended1 = await PollsAppServer.getPollExtended(pollID, userID);
+  const pollExtended2 = await PollsAppServer.getPollExtended(pollID, userID);
+  expect(pollExtended2.totalCount).toBe(pollExtended1.totalCount);
+});
