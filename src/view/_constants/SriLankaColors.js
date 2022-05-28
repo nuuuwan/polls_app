17b47df20f2356f833680adccd92bb55fd80fd65
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 11:05:11 +0530

    Added SriLankaColors

---
+++ src/view/_constants/SriLankaColors.js
@@ -0,0 +1,12 @@
+function rgb(r, g, b) {
+  return `rgb(${r},${g},${b})`;
+}
+
+const SriLankaColors = {
+  Buddhist: rgb(255, 190, 41),
+  Sinhala: rgb(141, 21, 58),
+  Tamil: rgb(235, 116, 0),
+  Muslim: rgb(0, 83, 78),
+};
+
+export default SriLankaColors;
