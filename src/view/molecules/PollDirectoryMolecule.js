5f23f99fafbedc0bd1d54c50968a2e1964fc134f
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 09:18:31 +0530

    Rename to PollDirectoryMolecule

--- src/view/molecules/PollDirectoryMolecule.js
+++ src/view/molecules/PollDirectoryMolecule.js
@@ -5,7 +5,10 @@ import Stack from "@mui/material/Stack";
 
 import PollTitle from "../../view/molecules/PollTitle";
 
-export default function PollDirectory({ pollExtendedIdx, onSelectPoll }) {
+export default function PollDirectoryMolecule({
+  pollExtendedIdx,
+  onSelectPoll,
+}) {
   return (
     <Stack spacing={1}>
       <List>
