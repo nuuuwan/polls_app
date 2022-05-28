7bbf9c24817ed05b28f375d301341da68b85f4f1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-28 09:17:48 +0530

    Make PollPage shallow; Do the having lifting in child organisms;

--- src/view/pages/PollPage.js
+++ src/view/pages/PollPage.js
@@ -1,13 +1,10 @@
 import { Component } from "react";
 
 import Box from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
 
 import { TimeX } from "@nuuuwan/utils-js-dev";
 
 import AudioX from "../../nonview/core/AudioX";
-import PollsAppServer from "../../nonview/core/PollsAppServer";
-import URLContext from "../../nonview/core/URLContext";
 
 import PollBottomNavigation from "../../view/organisms/PollBottomNavigation";
 import PollDirectory from "../../view/organisms/PollDirectory";
@@ -17,60 +14,38 @@ export default class PollPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pollIDs: undefined,
-      pollID: undefined,
       timeLastUpdated: 0,
     };
   }
 
   async componentDidMount() {
-    let { pollID } = URLContext.getContext();
-    if (!pollID) {
-      const pollIDs = await PollsAppServer.getPollIDs();
-      pollID = pollIDs[0];
-    }
-    this.refresh(pollID);
+    await this.refresh();
   }
 
-  refresh(pollID) {
+  async refresh() {
     this.setState({
-      pollID,
       timeLastUpdated: TimeX.getUnixTime(),
     });
-  }
-
-  async onSelectPoll(pollID) {
     await AudioX.playClick();
     window.scrollTo({ top: 0, behavior: "smooth" });
-    this.refresh(pollID);
   }
 
   render() {
-    const { pollID, timeLastUpdated } = this.state;
-    if (!pollID) {
-      return <CircularProgress />;
-    }
-    URLContext.setContext({ Page: PollPage, pollID });
-
+    const { timeLastUpdated } = this.state;
     return (
       <div>
         <Box sx={{ marginBotton: 1, maxWidth: "100%" }}>
           <PollView
-            key={"poll-view-" + pollID}
-            pollID={pollID}
-            onSelectPoll={this.onSelectPoll.bind(this)}
+            key={"poll-view-" + timeLastUpdated}
+            refresh={this.refresh.bind(this)}
           />
           <PollDirectory
             key={"poll-directory-" + timeLastUpdated}
-            onSelectPoll={this.onSelectPoll.bind(this)}
+            refresh={this.refresh.bind(this)}
           />
         </Box>
 
-        <PollBottomNavigation
-          key={"poll-bottom-navigation"}
-          pollID={pollID}
-          onSelectPoll={this.onSelectPoll.bind(this)}
-        />
+        <PollBottomNavigation refresh={this.refresh.bind(this)} />
       </div>
     );
   }
