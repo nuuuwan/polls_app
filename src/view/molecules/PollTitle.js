5f2242ef1c1898a95bfba2676576deb68ece0c66
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-29 10:41:21 +0530

    Lint only

--- src/view/molecules/PollTitle.js
+++ src/view/molecules/PollTitle.js
@@ -5,8 +5,8 @@ import { useTheme } from "@mui/material/styles";
 
 import AlignCenter from "../../view/atoms/AlignCenter";
 import PollStatistics from "../../view/molecules/PollStatistics";
-import PollVisibility from "../../view/molecules/PollVisibility";
 import PollUserAnswer from "../../view/molecules/PollUserAnswer";
+import PollVisibility from "../../view/molecules/PollVisibility";
 
 export default function PollTitle({ pollExtended, small }) {
   const isUserAnswer =
