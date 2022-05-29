0d4ffc39eefeb4bfddeb04e050b472941a0f8a60
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-29 10:16:27 +0530

    Use PollUserAnswer

--- src/view/molecules/PollTitle.js
+++ src/view/molecules/PollTitle.js
@@ -2,12 +2,11 @@ import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import AlignCenter from "../../view/atoms/AlignCenter";
-import Condition from "../../view/atoms/Condition";
 import PollStatistics from "../../view/molecules/PollStatistics";
 import PollVisibility from "../../view/molecules/PollVisibility";
+import PollUserAnswer from "../../view/molecules/PollUserAnswer"
 
 export default function PollTitle({ pollExtended, small }) {
   const isUserAnswer =
@@ -19,7 +18,6 @@ export default function PollTitle({ pollExtended, small }) {
     : theme.palette.primary.main;
 
   const fontSize = small ? "small" : "normal";
-  const fontSizeSmall = small ? "x-small" : "small";
 
   return (
     <Stack>
@@ -35,14 +33,7 @@ export default function PollTitle({ pollExtended, small }) {
         <PollVisibility pollExtended={pollExtended} small={small} />
       </AlignCenter>
 
-      <Condition condition={isUserAnswer}>
-        <AlignCenter>
-          <CheckCircleIcon sx={{ fontSize: fontSizeSmall, color: color }} />
-          <Typography style={{ fontSize: fontSizeSmall, color: color }}>
-            You voted "{pollExtended.userAnswer}"
-          </Typography>
-        </AlignCenter>
-      </Condition>
+      <PollUserAnswer pollExtended={pollExtended} />
     </Stack>
   );
 }
