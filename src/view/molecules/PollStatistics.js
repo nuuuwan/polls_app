db1f4f086a8079def50bc457ef9aa4bd6e7cfb21
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-29 09:53:25 +0530

    Use PollStatisticsTotal and PollStatisticsSignificance

--- src/view/molecules/PollStatistics.js
+++ src/view/molecules/PollStatistics.js
@@ -1,54 +1,11 @@
-import Divider from "@mui/material/Divider";
-import Typography from "@mui/material/Typography";
-
-import Statistics from "../../nonview/base/Statistics";
-
-import Condition from "../../view/atoms/Condition";
+import PollStatisticsSignificance from "../../view/atoms/PollStatisticsSignificance";
+import PollStatisticsTotal from "../../view/atoms/PollStatisticsTotal";
 
 export default function PollStatistics({ pollExtended, small }) {
-  const fontSize = small ? "x-small" : "small";
-  const renderedTotalVotes = (
-    <Typography style={{ fontSize }}>
-      {pollExtended.totalCount + " votes"}
-    </Typography>
-  );
-
-  let significanceStr = "";
-  if (pollExtended.totalCount > Statistics.MIN_STATISTICAL_N) {
-    const sortedAnswerStats = pollExtended.answerList
-      .map(function (answer) {
-        const answerVotes = pollExtended.answerToCount[answer];
-        const { lower, upper, p } = Statistics.getErrorBounds(
-          pollExtended.totalCount,
-          answerVotes
-        );
-        return { answer, lower, upper, p };
-      })
-      .sort(function (answerA, answerB) {
-        return answerB.p - answerA.p;
-      });
-
-    const topAnswerStats = sortedAnswerStats[0];
-    const nextAnswerStats = sortedAnswerStats[1];
-
-    significanceStr = "Too close to call";
-    const topLower = topAnswerStats.lower;
-    const nextUpper = nextAnswerStats.upper;
-    const gap = topLower - nextUpper;
-    if (gap > 0) {
-      significanceStr = `"${topAnswerStats.answer}" leads`;
-    }
-  }
-
   return (
     <>
-      {renderedTotalVotes}
-      <Condition condition={significanceStr}>
-        <Divider orientation="vertical" variant="middle" flexItem />
-        <Typography sx={{ fontSize: fontSize, color: "gray" }}>
-          {significanceStr}
-        </Typography>
-      </Condition>
+      <PollStatisticsTotal pollExtended={pollExtended} small={small} />
+      <PollStatisticsSignificance pollExtended={pollExtended} small={small} />
     </>
   );
 }
